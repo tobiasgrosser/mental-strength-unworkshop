213f4d9b6e58fbb189913d6b1216ad58bc276332
Tobias Grosser <tobias@grosser.es>
2021-06-03 08:24:54 +0100

    Update text

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -8,10 +8,10 @@ const Sidebar = ({ siteMetadata }) => (
     <aside className="sidebar">
       <header>
         <div className="about">
-          <div className="author-name">{siteMetadata.author}</div>
+          <div className="author-name"><a href="/">{siteMetadata.author}</a></div>
           <p>
-The un-workshops are organized for members by members. They provide a space for any topic relevant to the scientific excellence and well-being initiative. They are an opportunity to explore ideas, to practice, to discuss experiences, to exchange insights, to form support groups and to create something completely new, together.
-Join the mental-strength-unworkshop channel on our slack server to get informed about upcoming un-workshops.</p>
+The un-workshops are virtual workshops organized by students and researchers for students and researchers. They provide a space to practice and learn togther and allow us to explore ideas, to practice, to discuss experiences, to exchange insights, to form support groups and to create something completely new, together. 
+<></> <a href="https://join.slack.com/t/vmi-ethz/signup">Join</a> the mental-strength-unworkshop channel on our slack server to get informed about and co-develop upcoming un-workshops.</p>
         </div>
       </header>
       <footer>
