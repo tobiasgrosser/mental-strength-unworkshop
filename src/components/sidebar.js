1d141768fabbc87e5ef17df9c3ce38525210b9cc
Tobias Grosser <tobias@grosser.es>
2021-06-03 08:28:30 +0100

    Add information about organizers

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -3,6 +3,7 @@ import React from 'react'
 
 import Logo from './pic.jpg'
 
+
 const Sidebar = ({ siteMetadata }) => (
   <>
     <aside className="sidebar">
@@ -12,6 +13,10 @@ const Sidebar = ({ siteMetadata }) => (
           <p>
 The un-workshops are virtual workshops organized by students and researchers for students and researchers. They provide a space to practice and learn togther and allow us to explore ideas, to practice, to discuss experiences, to exchange insights, to form support groups and to create something completely new, together. 
 <></> <a href="https://join.slack.com/t/vmi-ethz/signup">Join</a> the mental-strength-unworkshop channel on our slack server to get informed about and co-develop upcoming un-workshops.</p>
+
+	<p>
+	The UnWorkshop series is co-organized between ETH Zurich and the University of Edinburgh.
+	</p>
         </div>
       </header>
       <footer>
