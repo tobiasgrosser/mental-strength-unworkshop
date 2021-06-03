8ae79b24309aa285059572285452b2fa95524f79
Tobias Grosser <tobias@grosser.es>
2021-06-03 08:41:46 +0100

    Add Google event calendar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -17,6 +17,11 @@ The un-workshops are virtual workshops organized by students and researchers for
 	<p>
 	The UnWorkshop series is co-organized between ETH Zurich and the University of Edinburgh.
 	</p>
+
+	<br />
+	<br />
+	<h3>Event Calendar</h3>
+	<iframe src="https://calendar.google.com/calendar/embed?height=500&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Europe%2FLondon&amp;src=MWRkdTZpcG1qaGtybXVzbHVjdGFrc2pjZXNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23B39DDB&amp;showCalendars=0&amp;showNav=0&amp;showPrint=0&amp;showDate=0&amp;showTitle=0&amp;showTz=1&amp;showTabs=0&amp;mode=AGENDA" width="100%" height="500" frameborder="0" scrolling="no"></iframe>
         </div>
       </header>
       <footer>
