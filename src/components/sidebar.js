242b774ae2bc217f792ec135339e6f7a65f9e257
Tobias Grosser <tobias@grosser.es>
2021-06-08 17:09:36 +0100

    Fix the Sign-Up Link

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -12,7 +12,7 @@ const Sidebar = ({ siteMetadata }) => (
           <div className="author-name"><a href="/">{siteMetadata.author}</a></div>
           <p>
 The un-workshops are virtual workshops organized by students and researchers for students and researchers. They provide a space to practice and learn togther and allow us to explore ideas, to practice, to discuss experiences, to exchange insights, to form support groups and to create something completely new, together. 
-<></> <a href="https://join.slack.com/t/vmi-ethz/signup">Join</a> the mental-strength-unworkshop channel on our slack server to get informed about and co-develop upcoming un-workshops.</p>
+<></> <a href="https://join.slack.com/t/mentalstrength/signup">Join</a> the mental-strength-unworkshop channel on our slack server to get informed about and co-develop upcoming un-workshops.</p>
 
 	<p>
 	The UnWorkshop series is co-organized between ETH Zurich and the University of Edinburgh.
